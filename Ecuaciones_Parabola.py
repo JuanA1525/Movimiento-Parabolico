ae387fce0eba62c6010fa9601f0179ab9c603479
Juan Andres Porras <juana.porras@upb.edu.co>
2022-08-26 00:39:20 -0500

    VERSION TERMINADA DEL CODIGO. Y ELIMINACION DE ARCHIVO INNECESARIO

--- Ecuaciones_Parabola.py
+++ Ecuaciones_Parabola.py
@@ -1,3 +1,4 @@
+from logging import exception
 from cProfile import label
 import tkinter
 from tkinter.ttk import LabeledScale
@@ -10,23 +11,21 @@ import json
 
 
 
-#@title DATOS NECESARIOS (Ojo, el angulo es necesario):
+#@title DIngreso de Datos: Angulo necesario, Tmax o Vi Necesarios.
 
 try:
   
-  angulo = 12 #@param {type:"number"}
+  angulo = 70 #@param {type:"number"}
   #@markdown Ingrese la velocidad inicial:
-  Vi = 0 #@param {type:"number"}
+  Vi = 3 #@param {type:"number"}
   #@markdown Ingrese la altura inicial:
-  Hi = 2 #@param {type:"number"}
+  Hi = 0.263 #@param {type:"number"}
   #@markdown Ingrese la altura final:
-  Hf = -6 #@param {type:"number"}
+  Hf = None #@param {type:"number"}
   #@markdown Ingrese la posicion inicial:
-  Xi = 0 #@param {type:"number"}
-  #@markdown Ingrese la posicion final:
-  Xf = 0 #@param {type:"number"}
+  Xi = None #@param {type:"number"}
   #@markdown Ingrese el tiempo maximo:
-  tMax = 6 #@param {type:"number"}
+  tMax = None #@param {type:"number"}
 
   g = 9.776
 
@@ -36,11 +35,8 @@ try:
   if Xi == None:
     Xi = 0
 
-  if Xf == None:
-    Xf = 0
-
   if Hf == None:
-    Hf == 0
+    Hf = 0
 
   if tMax == None:
     tMax = 0
@@ -51,15 +47,14 @@ try:
   if Vi == 0 or Vi == None:
     Vi = (g*tMax)/2*np.sin(alpha)
 
-  raiz = (((Vi**2)*(np.sin(alpha)**2)) + 2*g*Hi)
+  raiz = (((Vi**2)*(np.sin(alpha)**2)) - (2*g*Hf) + (2*g*Hi))
 
   if raiz < 0:
-      print(raiz)
-      print("\nTu Parabola NO sale de la tierra. Intenta con otros valores\n")
+      print("\nNO ES POSIBLE CALCULAR UNA PARABOLA CON LOS DATOS INGRESADOS.\nINTENTA CON OTROS VALORES")
 
   else:
 
-    tMax2 = (((Vi*np.sin(alpha)) + (np.sqrt(((Vi**2)*(np.sin(alpha)**2)) -2*g*Hf + 2*g*Hi))) / g)
+    tMax2 = (((Vi*np.sin(alpha)) + (np.sqrt(((Vi**2)*(np.sin(alpha)**2)) - (2*g*Hf) + (2*g*Hi)))) / g)
     t = np.arange(0, tMax2+tMax2/50, tMax2/50)
 
 
@@ -75,32 +70,57 @@ try:
     xHmax = Xi + (Vx*tHmax)
 
     tXmax = tMax2
+
     xXmax = Xi + (Vx*tMax2)
+    xYmax = (((Vi*np.sin(alpha)*tMax2) - (0.5*g*tMax2**2)) + Hi)
 
-    fig, ax = plt.subplots(1,2, figsize = (15,5))
-    ax[0].plot(X, Y,'k--',lw=3)
-    ax[0].set_title("Trayectoria (x,y)", fontsize=25)
-    ax[0].set_xlabel('x [m]', fontsize=16)
-    ax[0].set_ylabel('y [m]', fontsize=16)
-    ax[0].grid(True, which='both')
-    ax[0].plot(xHmax,Ymax,'s',lw=4, label = (f'Altura Max: ({np.round(xHmax,2)}, {np.round(Ymax,2)})'))
-    ax[0].plot(xXmax,Hf,'o',lw=4, label=f'Distancia Max: ({np.round(xXmax,2)}, {Hf})')
-    ax[0].plot(Xi,Hi,'v',lw=4,label=f'Posicion Inicial: ({Xi}, {Hi})')
-    ax[0].plot(lw=4,label=f'Velocidad Generada: ({np.round(Vi, 2)})')
-    ax[0].legend()
+    Vxf = Vx
+    Vyf = ((Vi*np.sin(alpha)) - (g*tMax2))
+    Vf = np.sqrt((Vxf**2) + (Vxf**2))
 
-    ax[1].plot(X, Y,'k--',lw=3)
-    ax[1].set_title("Trayectoria (x,y)", fontsize=25)
-    ax[1].set_xlabel('x [m]', fontsize=16)
-    ax[1].set_ylabel('y [m]', fontsize=16)
-    ax[1].grid(True, which='both')
-    ax[1].plot(xHmax,Ymax,'s',lw=4, label = (f'Altura Max: ({np.round(xHmax,2)}, {np.round(Ymax,2)})'))
-    ax[1].plot(xXmax,Hf,'o',lw=4, label=f'Distancia Max: ({np.round(xXmax,2)}, {Hf})')
-    ax[1].plot(Xi,Hi,'v',lw=4,label=f'Posicion Inicial: ({Xi}, {Hi})')
-    ax[1].plot('o', lw=4,label=f'Velocidad Generada: ({np.round(Vi, 2)})')
-    ax[1].legend()
+    angFinal = (((np.arctan((Vyf)/(Vxf))) * 180) / np.pi)
+
+    fig, ax = plt.subplots(1,1, figsize = (15,5))
+    ax.plot(X, Y,'k--',lw=3)
+    ax.set_title("Trayectoria (x,y)", fontsize=25)
+    ax.set_xlabel('x [m]', fontsize=16)
+    ax.set_ylabel('y [m]', fontsize=16)
+    ax.grid(True, which='both')
+    ax.plot(xHmax,Ymax,'s',lw=4, label = (f'Altura Max: ({np.round(xHmax,2)}, {np.round(Ymax,2)})'))
+    ax.plot(xXmax,Hf,'o',lw=4, label=f'Distancia Max: ({np.round(xXmax,2)}, {Hf})')
+    ax.plot(Xi,Hi,'v',lw=4,label=f'Posicion Inicial: ({Xi}, {Hi})')
+    ax.plot(lw=4,label=f'Velocidad Generada: ({np.round(Vi, 2)})')
+    ax.legend()
 
     fig.show()
-    input("")
-except:
+
+    print(f"""  
+  De acuerdo a los datos ingresados, los datos Finales son:
+
+    TIEMPOS:
+      - Tiempo de Vuelo: ({np.round(tMax2,2)} s)
+      - Tiempo para Altura Maxima: ({np.round(tHmax,2)} s)
+
+    VELOCIDADES:
+      - Velocidad Inicial: ({Vi} m/s)
+      - Velocidad Final: ({np.round(Vf,2)} m/s)
+
+    POSICIONES:
+      - Posicion Inicial: ({np.round(Xi,2)}, {np.round(Hi,2)})
+      - Posicion Final: ({np.round(xXmax,2)}, {np.round(xYmax,2)})
+
+    DISTANCIAS:
+      - Distancia Maxima Recorrida: ({np.round(xXmax,2)} m)
+      - Altura Maxima Alcanzada: ({np.round(Ymax,2)} m)
+
+    ANGULOS:
+      - Ángulo Inicial: ({angulo})
+      - Ángulo Final: ({np.round(angFinal,2)})
+    
+  Gracias Por Utilizar Nuestro Sistema. 
+    """)
+except(exception):
+  print(exception)
   print("No se puede calcular debido a la falta de datos necesarios.")
+
+input("")
